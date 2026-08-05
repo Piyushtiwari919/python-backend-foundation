d4c36556287d71881dc9a3658084675a26023d0a
Piyush Tiwari <piyushtiwari919921@gmail.com>
2026-08-05 12:23:25 +0530

    feat: add cpu_bound.py for handling CPU-bound tasks with asyncio - Using asyncio.to_thread() to execute the cpu bound task without blocking the event loop

---
+++ Asyncio_GIL/cpu_bound.py
@@ -0,0 +1,37 @@
+import asyncio
+
+
+def chunk_massive_document() -> int:
+    """A synchronous CPU-bound task simulating heavy text processing"""
+    print("[CPU] Starting heavy computation (locking the CPU)...")
+
+    result: int = sum(i * i for i in range(30000000))
+
+    print("[CPU] Finished heavy computation!")
+    return result
+
+
+async def serve_user(id: int, cpuTask: bool) -> None:
+    print(f"User: {id} requested")
+    if cpuTask:
+        res = await asyncio.to_thread(chunk_massive_document)
+        print(f"The output of User: {id} task is {res}")
+        print(f"User: {id} request completed")
+    else:
+        print(f"User: {id} requested a fast profile load.")
+
+        await asyncio.sleep(0.1)
+
+        print(f"User: {id} Profile load complete.")
+
+
+async def main():
+    print("--- Server Started ---")
+    try:
+        await asyncio.gather(serve_user(1, True), serve_user(2, False))
+    except Exception as e:
+        print(f"Exception occured: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
